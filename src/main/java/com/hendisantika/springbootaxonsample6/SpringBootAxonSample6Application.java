a5d44288e0664667daf27b6bffe9a20d26a9711a
Hendi Santika <hendisantika@gmail.com>
2022-01-16 20:01:18 +0700

    #20 - Add Exception Handling

--- src/main/java/com/hendisantika/springbootaxonsample6/SpringBootAxonSample6Application.java
+++ src/main/java/com/hendisantika/springbootaxonsample6/SpringBootAxonSample6Application.java
@@ -1,5 +1,8 @@
 package com.hendisantika.springbootaxonsample6;
 
+import com.hendisantika.springbootaxonsample6.command.exception.ProductServiceEventsErrorHandler;
+import org.axonframework.config.EventProcessingConfigurer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,4 +13,11 @@ public class SpringBootAxonSample6Application {
         SpringApplication.run(SpringBootAxonSample6Application.class, args);
     }
 
+    @Autowired
+    public void configure(EventProcessingConfigurer configurer) {
+        configurer.registerListenerInvocationErrorHandler(
+                "product",
+                configuration -> new ProductServiceEventsErrorHandler()
+        );
+    }
 }
