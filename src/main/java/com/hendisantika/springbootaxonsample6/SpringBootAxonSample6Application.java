714edd4232cc8f5bdef6d91eeb2aa2e6b4400907
Hendi Santika <hendisantika@gmail.com>
2022-01-16 19:32:44 +0700

    #1 - Initial commit

---
+++ src/main/java/com/hendisantika/springbootaxonsample6/SpringBootAxonSample6Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootaxonsample6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAxonSample6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAxonSample6Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootaxonsample6/SpringBootAxonSample6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootaxonsample6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAxonSample6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
