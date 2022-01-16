16e25b7bcbbfa1ed4d16c8a343ae0a48a3442747
Hendi Santika <hendisantika@gmail.com>
2022-01-16 19:43:29 +0700

    #15 - Add GetProductsQuery

---
+++ src/main/java/com/hendisantika/springbootaxonsample6/query/query/GetProductsQuery.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootaxonsample6.query.query;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-axon-sample6
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/01/22
+ * Time: 19.43
+ */
+public class GetProductsQuery {
+}
